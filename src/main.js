b011dce7c26607c3890b9e634e9aca42dd126912
Joe Shepherd <joeshepmedia@gmail.com>
2019-06-17 14:55:50 -0500

    Add DOM interactions and fetching of original data

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
 console.log('POSTing for fun and profit');
+let legoList = document.querySelector("#lego-list")
+
+// Get the data from the db and add it to the DOM
+function getLegos() {
+  fetch("http://localhost:8088/legos")
+  .then( legoData => legoData.json())
+  .then( legoData => {
+    legoData.forEach( lego => {
+      legoList.innerHTML += `<li>${lego.creator} made a ${lego.color} thingy with legos!</li>`
+    })
+  })
+}
+getLegos()
 
 document.querySelector("#lego-btn").addEventListener("click", () => {
   let creatorName = document.querySelector("#lego-creator").value
   let color = document.querySelector("#lego-color").value
-  let newLego = {
-    creator: creatorName,
-    color: color
-  }
+  // example of calling a factory function
+  let newLego = buildLegoObj(creatorName, color)
   addNewLegoCreation(newLego)
+  .then( data => data.json())
+  .then( dataJS => {
+    console.log(dataJS.color)
+    legoList.innerHTML = ""
+    getLegos()
+  })
 })
 
 function addNewLegoCreation(creation) {
-  fetch("http://localhost:8088/legos", {
+  console.log("no refresh")
+  return fetch("http://localhost:8088/legos", {
     method: "POST",
     headers:{
       "Content-Type": "application/json"
@@ -34,5 +52,13 @@ function updateLego(id, shape, creation) {
       },
       body: JSON.stringify(lego)
     })
+  })
+}
+
+// This is a factory function, because it returns an object
+function buildLegoObj(creatorName, color) {
+  return ({
+    creator: creatorName,
+    color: color
   })
 }
